d227f58b3c04ef3d54a442419da5ffaa699261d6
abdulfarid.atiq@cba.ku.edu.kw <abdulfarid.atiq@cba.ku.edu.kw>
2023-11-16 20:42:26 +0300

    End of the task!

--- vehicles/cars/admin.py
+++ vehicles/cars/admin.py
@@ -9,10 +9,12 @@ class CarInLine(admin.TabularInline):
 
 class PersonAdmin(admin.ModelAdmin):
   list_display=("driver_id", "name", "address")
+  search_fields = ("name", "model")
   inlines = (CarInLine,)
 
 class CarAdmin(admin.ModelAdmin):
   list_display=("license", "model", "year", "person")
+  search_fields = ("name", "model")
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Car, CarAdmin)
