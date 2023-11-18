660fb305c92d7f306539d269cd4318955dd0a4e5
sp2ctator <146710045+sp2ctator@users.noreply.github.com>
2023-11-18 22:35:11 +0300

    done the bonus assigment

--- vehicles/cars/views.py
+++ vehicles/cars/views.py
@@ -11,3 +11,38 @@ def read_cars(request):
   all_cars = Car.objects.all()
   data['allc'] = all_cars
   return render(request, "read_cars.html", context=data)
+
+def list_bmw(request):
+  data={}
+  all_cars = Car.objects.filter(model="bmw")
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def list_gte(request):
+  data={}
+  all_cars = Car.objects.filter(year__gte="200")
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def list_between(request):
+  data={}
+  all_cars = Car.objects.filter(year__range=["1999","2000"])
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+
+def list_by_name(request):
+  data={}
+
+  all_cars = p.car_set.all()
+  p = Person.objects.get(name='Mohammad Jamal')
+  data['person'] = p
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def search_by_license(request, license_id):
+  p = Car.objects.get(license= license_id)
+  data={}
+  #data['allc'] = p
+  data = {'allc': p}
+  return render(request, "car_search.html", data)
