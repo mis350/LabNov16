722db50711eeeec6bdffa9ad21d2d961082ce9cc
lailahaidar <145322841+lailahaidar@users.noreply.github.com>
2023-11-22 11:10:08 +0300

    solution

--- vehicles/cars/urls.py
+++ vehicles/cars/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.greeting),
-    path('readc/', views.read_cars),
+    path('', views.greeting, name='greeting'),
+    path('readc/', views.read_cars, name='allcars'),
+    path('list_bmw_cars/', views.list_bmw_cars, name='bmws'),
+    path('list_2000_cars/', views.list_2000_cars),
+    path('list_1990_2000/', views.list_1990_2000),
+    path('list_cars_Mohammad_Jamal/', views.list_cars_Mohammad_Jamal),
+    path('search_car/<str:search_query>', views.search_car, name='search'),
+    
 ]
--- vehicles/cars/views.py
+++ vehicles/cars/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SearchForm
 from .models import Person, Car
 
 # Create your views here.
 def greeting(request):
   data={}
+  if request.method == 'POST':
+    form = SearchForm(request.POST)
+    if form.is_valid():
+      search_query = form.cleaned_data['search_query']
+      return redirect('search', search_query=search_query)
+    else:
+        form = SearchForm()
+        return render(request, 'greeting.html', {'form': form})
+
   return render(request, "greeting.html", context=data)
 
 def read_cars(request):
@@ -11,3 +21,38 @@ def read_cars(request):
   all_cars = Car.objects.all()
   data['allc'] = all_cars
   return render(request, "read_cars.html", context=data)
+
+def list_bmw_cars(request):
+  data={}
+  all_cars = Car.objects.filter(model="BMW")
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def list_2000_cars(request):
+  data={}
+  all_cars = Car.objects.filter(year__gte=2000)
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def list_1990_2000(request):
+  data={}
+  all_cars = Car.objects.filter(year__range=["1990", "2000"])
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def list_cars_Mohammad_Jamal(request):
+  data={}
+  person_record = Person.objects.get(name = "Mohammad Jamal")
+  all_cars = person_record.car_set.all()
+  data['person'] = person_record
+  data['allc'] = all_cars
+  return render(request, "read_cars.html", context=data)
+
+def search_car(request, search_query):
+
+    results = Car.objects.filter(license__icontains=search_query)
+    data={'results': results, 'search_query': search_query}
+    car_record = Car.objects.get(license = search_query)
+    data['allc'] = car_record
+
+    return render(request, 'search_car.html', context = data)
